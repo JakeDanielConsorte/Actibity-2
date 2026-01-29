f13644455b2ce061a70ffafa22cad00a1669ea8b
JakeDanielConsorte <jakedaniel.consorte@gmail.com>
2026-01-29 12:02:09 +0800

    Bus.java

--- Bus.java
+++ Bus.java
@@ -1,27 +1,29 @@
 public class Bus {
-    //Aggregation kasi from other class (outside)
-    Driver driver;
-    //Composition kasi inside
-    Engine eng = new Engine("Diesel");
+  
+    public Driver driver;
+    public Engine engine;
 
     Bus(Driver driver){
         this.driver = driver;
+        this.engine = new Engine("Diesel");
     }
 
-    String identity(){
-        return "I am the bus. I use a driver, boards passenger and owns an engine.";
+    public String engineIdentity(){
+        return engine.identity();
     }
 
     public static void main(String[] args){
-        Driver driver = new Driver("Jake");
+
+        Driver driver = new Driver(10);
+
         Passenger passenger = new Passenger("Student");
+
         Bus bus = new Bus(driver);
 
         String output =
                 driver.identity() + "\n" +
                 passenger.identity() + "\n" +
-                bus.identity() + "\n" +
-                bus.eng.identity();
+                bus.engine.identity(); 
 
         System.out.println(output);
 
