d3b1c1d805294ebffc418834f457f789ce9ab846
JakeDanielConsorte <jakedaniel.consorte@gmail.com>
2026-01-29 12:03:02 +0800

    Engine.java

--- Engine.java
+++ Engine.java
@@ -1,12 +1,17 @@
 public class Engine {
+
     String fuelType;
 
     Engine(String fuelType){
         this.fuelType = fuelType;
     }
 
+    String startEngine(){
+        return "I have started and I am ready to accelerate.";
+    }
+
     String identity(){
-        return "I am the Engine. " + fuelType +" is my fuel type";
+        return "I am the Engine. " + fuelType +" is my fuel type. " + startEngine();
     }
 
 }
