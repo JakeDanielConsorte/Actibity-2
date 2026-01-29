23c2938453f345fb7af9c5fa9c2384a2d024517f
JakeDanielConsorte <jakedaniel.consorte@gmail.com>
2026-01-29 12:03:18 +0800

    Update Passenger.java

--- Passenger.java
+++ Passenger.java
@@ -1,12 +1,17 @@
 public class Passenger {
+
     String ticketType;
 
     Passenger(String ticketType){
         this.ticketType = ticketType;
     }
 
+    String board(){
+        return "I board the bus.";
+    }
+
     String identity(){
-        return "I am the passenger. Ticket type: " + ticketType + ".";
+        return "I am the passenger." + " I pay "+ ticketType + " fee. " + board() ;
     }
 
 }
